63a6790ef1849c13aca24bf36e2b08dc578595cd
aliharby12 <aliharby565@gmail.com>
2020-12-18 09:22:26 +0200

    create dummy data for the products and handle the routs

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,21 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
+    <h2>Products</h2>
+    <div class="row d-flex justify-content-center">
+        @foreach ($products as $product)
+            <div class="card col-md-3 m-2">
+                <img class="card-img-top" src="#" alt="Card image cap">
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    <h4 class="card-title">{{ $product->name }}</h4>
+                    <p class="card-text">{{ $product->description }}</p>
+                </div>
+                <div class="card-body d-flex justify-content-center">
+                    <a class="btn btn-primary" href="#" class="card-link">Add To Cart</a>
+                    <a class="btn btn-info" href="#" class="card-link">Show Product</a>
                 </div>
             </div>
-        </div>
+        @endforeach
     </div>
 </div>
 @endsection
